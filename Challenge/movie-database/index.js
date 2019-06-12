be6c396e0688b15461bf2b585199f5a91e4799ee
ridaFD <ridafakherlden@gmail.com>
2019-06-12 13:19:26 +0300

    Step 5 - Set up the basis for CRUD

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -31,4 +31,15 @@ app.get('/hello/:ID', (req, res) => {
                 res.send({status:500, error:true, message:"you have to provide a search"}) 
             } })    
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+app.get('/movies/read',(req,res) => {
+    res.send({status:200, data:movies})
+})
+
 app.listen(3000, () => console.log('listinig on port 3000'))
